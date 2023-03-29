a3b8591a6755cdee8d8009651413bf7910017069
MundiaNderi <sarahngima77@gmail.com>
2023-03-29 14:40:09 +0300

    Fixing betty

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -10,8 +10,8 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 {
 size_t count;
 if (tree == NULL || tree->parent == NULL)
-return(0);
+return (0);
 count = binary_tree_depth(tree->parent);
 count++;
-return count;
+return (count);
 }
